23e465963cb46ff481045bedc13ae9bf390b9394
FDSantiago <mfdsantiago@tip.edu.ph>
2024-09-10 09:35:23 +0800

    initial commit

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+const int EMPLOYEES_NUM = 4;
+
+int main() {
+    int employeeSalaries[EMPLOYEES_NUM];
+    int taxPercentage;
+
+    for (int i = 0; i < EMPLOYEES_NUM; i++) {
+        const int realNum = i + 1;
+
+        cout << "Enter employee #" << realNum << "'s salary: ";
+        cin >> employeeSalaries[i];
+    }
+
+    cout << "Whats the tax percentage? (0 - 100): ";
+    cin >> taxPercentage;
+
+    cout << "\n--=[ Results ]=--\n";
+    
+    for (int i = 0; i < EMPLOYEES_NUM; i++) {
+        cout << "Employee #" << i << " has a salary of $" << employeeSalaries[i] << "\n";
+        
+        if (employeeSalaries[i] > 10000) {
+            const int taxedAmount = employeeSalaries[i] * taxPercentage / 100;
+            cout << "Tax = " << taxPercentage << "% of $" << employeeSalaries[i] << " = $" << taxedAmount;
+        } else {
+            cout << "This employee does not qualify for tax.";
+        }
+        
+        cout << "\n\n";
+    }
+
+    return 0;
+}
